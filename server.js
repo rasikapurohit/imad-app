46a5cf2e239893707db3798eb7429b2c20034c3d
Rasika Purohit <rasikasrp@gmail.com>
2017-09-06 22:10:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get('/article-two', function(req, res){
 });
 
 app.get('/article-three', function(req, res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+    res.sendFile(path.join(__dirname, 'ui', 'artice-three.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
