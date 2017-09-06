7b6b3d7139e4719a137b7818ffa4deade39bc007
Rasika Purohit <rasikasrp@gmail.com>
2017-09-06 22:09:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,11 +13,11 @@ app.get('/article-one', function(req, res){
 });
 
 app.get('/article-two', function(req, res){
-   res.send('Article two will be requested andsend here') ;
+   res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 
 app.get('/article-three', function(req, res){
-   res.send('Article three will be requested andsend here') ;
+    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
